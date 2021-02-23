81729f2eb513978f895c40296a1d725ed0444acc
Fabricyo Barreto <bass.barreto@gmail.com>
2021-02-23 10:08:44 -0300

    Migration: Foi feita a migration que cria a tabela users no banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
 
@@ -9,4 +10,4 @@ createConnection();
 
 app.get('/users', (request, response) => response.send('Olá mundo !'));
 
-app.listen(process.env.PORT || 3333, () => console.log('Server is running...'))
+app.listen(process.env.PORT || 3333, () => console.log('Server is running...'));
