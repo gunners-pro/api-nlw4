6bc681c3fb182d65bbc24d6e4282f3aafa06f3f8
Fabricyo Barreto <bass.barreto@gmail.com>
2021-02-27 11:24:37 -0300

    AnswerController: Foi criado o controller e alterado a resposta do usuário no banco de dados.

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import SurveyController from './controllers/SurveyController';
 import UserController from './controllers/UserController';
 import SendMailController from './controllers/SendMailController';
+import AnswerController from './controllers/AnswerController';
 
 const Routes = Router();
 
@@ -9,5 +10,6 @@ Routes.post('/users', UserController.create);
 Routes.get('/surveys', SurveyController.show);
 Routes.post('/surveys', SurveyController.create);
 Routes.post('/sendMail', SendMailController.execute);
+Routes.get('/answer/:value', AnswerController.execute);
 
 export default Routes;
